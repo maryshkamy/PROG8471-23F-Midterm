7a2329e8bfacc54722a09459cd612d946758c9ce
Mariana Rios Silveira Carvalho <marianariossc@gmail.com>
2023-10-27 21:52:23 -0400

    feat: Add Midterm Xcode project. Set project to iPhone only.

---
+++ Mariana_RiosSilveiraCarvalho_MT_ 8903346/Mariana_RiosSilveiraCarvalho_MT_ 8903346/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mariana_RiosSilveiraCarvalho_MT_ 8903346
+//
+//  Created by Mariana Rios Silveira Carvalho on 2023-10-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
